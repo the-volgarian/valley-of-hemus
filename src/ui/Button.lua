2b2ba02ef1ef5efc12b67bc3143fe756781c7ca4
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2026-07-16 11:05:07 +0300

    feat(ui): add button hover detection

--- src/ui/Button.lua
+++ src/ui/Button.lua
@@ -1,7 +1,7 @@
 local Button = {}
 Button.__index = Button
 
-function Button.new(x,y,width,height,text,font)
+function Button.new(x,y,width,height,text,font,bgColor,textColor,hoverColor)
 
     local self = setmetatable({},Button)
 
@@ -11,20 +11,32 @@ function Button.new(x,y,width,height,text,font)
     self.height = height
     self.text = text
     self.font = font
-    
+    self.bgColor = bgColor
+    self.textColor = textColor
+    self.hoverColor = hoverColor
     self.textObject = love.graphics.newText(font, text)
 
     return self
 end
 
 function Button:draw()
-    love.graphics.setColor(1, 0, 0)
+    love.graphics.setColor(self.bgColor)
     love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
-
-    love.graphics.setColor(1, 1, 1)
+    love.graphics.setColor(self.textColor)
+    if self:isHovered() then
+        love.graphics.setColor(self.hoverColor)
+    end
     love.graphics.draw(self.textObject, self.x + 10, self.y + 8)
-
     love.graphics.setColor(1, 1, 1)
+end
+
+function Button:isHovered()
+    local mx, my = love.mouse.getPosition()
+    if mx >= self.x and mx <= self.x + self.width and my >= self.y and my <= self.y + self.height then
+        return true
+    else
+        return false
+    end
 end
 
 return Button
