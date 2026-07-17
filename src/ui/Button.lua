32be118cfcd85b7bdbbb8ab47074886fe2bae5b4
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2026-07-17 09:32:20 +0300

    refactor(ui): reorganize button component

--- src/ui/Button.lua
+++ src/ui/Button.lua
@@ -1,9 +1,8 @@
 local Button = {}
 Button.__index = Button
 
-function Button.new(x,y,width,height,text,font,bgImagePath,bgColor,textColor,pressedColor,hoverColor,onClick)
-
-    local self = setmetatable({},Button)
+function Button.new(x, y, width, height, text, font, bgImagePath, bgColor, textColor, pressedColor, hoverColor, onClick)
+    local self = setmetatable({}, Button)
 
     self.x = x
     self.y = y
@@ -15,11 +14,12 @@ function Button.new(x,y,width,height,text,font,bgImagePath,bgColor,textColor,pre
     self.bgColor = bgColor
     self.textColor = textColor
     self.hoverColor = hoverColor
+    self.pressedColor = pressedColor
+    self.onClick = onClick
+    self.pressed = false
+
     self.textObject = love.graphics.newText(self.font, self.text)
     self.bgImage = nil
-    self.onClick = onClick
-    self.pressedColor = pressedColor
-    self.pressed = false
 
     if self.bgImagePath then
         self.bgImage = love.graphics.newImage(self.bgImagePath)
@@ -30,11 +30,12 @@ end
 
 function Button:draw()
     local hovered = self:isHovered()
+
     local centerX = self.x + self.width / 2
     local centerY = self.y + self.height / 2
+
     local textX = self.x + (self.width - self.textObject:getWidth()) / 2
     local textY = self.y + (self.height - self.textObject:getHeight()) / 2
-
 
     love.graphics.push()
 
@@ -44,23 +45,18 @@ function Button:draw()
         love.graphics.translate(-centerX, -centerY)
     end
 
-    if hovered then
-        love.graphics.setColor(self.hoverColor)
-    else
-        love.graphics.setColor(self.bgColor)
-    end
-
-  
-    
     if self.bgImage then
+        love.graphics.setColor(1, 1, 1)
+
         local scaleX = self.width / self.bgImage:getWidth()
         local scaleY = self.height / self.bgImage:getHeight()
 
-        love.graphics.draw(self.bgImage,self.x,self.y,0,scaleX,scaleY)
+        love.graphics.draw(self.bgImage, self.x, self.y, 0, scaleX, scaleY)
     else
-        love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
+        love.graphics.setColor(hovered and self.hoverColor or self.bgColor)
+        love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
     end
-    
+
     love.graphics.setColor(self.textColor)
     love.graphics.draw(self.textObject, textX, textY)
 
@@ -82,6 +78,7 @@ end
 
 function Button:click()
     self.pressed = true
+
     if self.onClick then
         self.onClick()
     end
