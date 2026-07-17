c8c69f39a73001f0a0b5e186076eb46d2cea3c63
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2026-07-17 09:39:31 +0300

    feat(config): add initial LÖVE configuration

---
+++ conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+    t.identity = "ValleyOfHemus"
+
+    t.window.title = "Valley of Hemus"
+    t.window.width = 1280
+    t.window.height = 720
+
+    t.window.resizable = false
+    t.window.fullscreen = false
+    t.window.vsync = 1
+
+   -- t.window.icon = "iconPath"
+end
