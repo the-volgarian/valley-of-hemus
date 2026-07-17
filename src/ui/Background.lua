5e926c7e45492587442b76b6d2fd42ed879e4921
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2026-07-17 09:07:06 +0300

    feat(ui): add reusable background component

---
+++ src/ui/Background.lua
@@ -0,0 +1,42 @@
+local Background = {}
+Background.__index = Background
+
+function Background.new(bgImagePath)
+    local self = setmetatable({},Background)
+    
+    self.bgImagePath = bgImagePath
+    self.bgImage = love.graphics.newImage(self.bgImagePath)
+    self.bgImageWidth = self.bgImage:getWidth()
+    self.bgImageHeight = self.bgImage:getHeight()
+    self.screenWidth = love.graphics.getWidth()
+    self.screenHeight = love.graphics.getHeight()
+
+    return self
+end
+
+function Background:draw()
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+
+    local scaleX = screenWidth / self.bgImageWidth
+    local scaleY = screenHeight / self.bgImageHeight
+
+    local scale = math.max(scaleX, scaleY)
+
+    local drawWidth = self.bgImageWidth * scale
+    local drawHeight = self.bgImageHeight * scale
+
+    local x = (screenWidth - drawWidth) / 2
+    local y = (screenHeight - drawHeight) / 2
+
+    love.graphics.draw(
+        self.bgImage,
+        x,
+        y,
+        0,
+        scale,
+        scale
+    )
+end
+
+return Background
