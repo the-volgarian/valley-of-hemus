26833d66ee915c776d89cf57eb56b817a321bf25
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2026-07-20 21:44:16 +0300

    feat(commands): add attack command

---
+++ src/commands/AttackCommand.lua
@@ -0,0 +1,27 @@
+local QueryExecutor = require("database.QueryExecutor")
+
+local AttackCommand = {}
+
+function AttackCommand.execute(arguments)
+    local targetType = arguments[1]
+    local targetId = tonumber(arguments[2])
+
+    if targetType ~= "monster" then
+        error("Attack command supports only monster targets.")
+    end
+
+    if not targetId then
+        error("Usage: attack monster <id>")
+    end
+
+    QueryExecutor.execute(
+        "database/command/attack.sql",
+        {
+            playerId = 1,
+            targetType = targetType,
+            targetId = targetId
+        }
+    )
+end
+
+return AttackCommand
