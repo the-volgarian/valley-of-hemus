e7be09c26d3e4959bffc8260049c9602e0c9b660
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2026-07-27 23:53:40 +0300

    feat(config): update game configuration

--- conf.lua
+++ conf.lua
@@ -2,12 +2,15 @@ function love.conf(t)
     t.identity = "ValleyOfHemus"
 
     t.window.title = "Valley of Hemus"
-    t.window.width = 1280
-    t.window.height = 720
+
+    t.window.width = 1920
+    t.window.height = 1080
+
+    t.window.fullscreen = true
+    t.window.fullscreentype = "exclusive"
 
     t.window.resizable = false
-    t.window.fullscreen = false
     t.window.vsync = 1
 
-   -- t.window.icon = "iconPath"
+    t.window.usedpiscale = false
 end
