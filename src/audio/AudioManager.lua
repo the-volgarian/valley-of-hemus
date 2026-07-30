0d5b153f1ee285dc4c275a06717c0360bfc2c3ce
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2026-07-30 19:46:02 +0300

    feat(audio): add music volume update for all tracks

--- src/audio/AudioManager.lua
+++ src/audio/AudioManager.lua
@@ -6,12 +6,20 @@ local masterVolume = 1
 local musicVolume = 1
 local sfxVolume = 1
 
+local function updateMusicVolume()
+    for _, source in pairs(music) do
+        source:setVolume(musicVolume * masterVolume)
+    end
+end
+
 function AudioManager.load()
-    music.main_menu = love.audio.newSource("src/audio/test.mp3", "stream")
+    music.main_menu = love.audio.newSource("assets/audio/music/main_menu.mp3", "stream")
     music.main_menu:setLooping(true)
 
-    sound.button_select = love.audio.newSource("src/audio/button_select_sound.wav","static")
-    sound.button_hover = love.audio.newSource("src/audio/button_hover_sound.wav","static")
+    sound.button_select = love.audio.newSource("assets/audio/sfx/ui/button_select_sound.wav","static")
+    sound.button_hover = love.audio.newSource("assets/audio/sfx/ui/button_hover_sound.wav","static")
+
+    updateMusicVolume()
 
 end
 
@@ -48,7 +56,7 @@ function AudioManager.decreaseMusicVolume()
     end
     musicVolume = musicVolume - 0.1
     
-    love.audio.setVolume(musicVolume)
+    updateMusicVolume()
 end
 
 function AudioManager.increaseMusicVolume()
@@ -57,7 +65,7 @@ function AudioManager.increaseMusicVolume()
     end
     musicVolume = musicVolume + 0.1
     
-    love.audio.setVolume(musicVolume)
+     updateMusicVolume()
 end
 
 function AudioManager.decreaseSfxVolume()
